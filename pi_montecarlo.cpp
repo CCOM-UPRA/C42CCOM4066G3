432f2e75fba52865035751def0201b15b27140a4
REGM8814 <158120508+REGM8814@users.noreply.github.com>
2025-05-07 00:42:46 +0000

    Fix errors
    Why is this method named Monte Carlo

--- pi_montecarlo.cpp
+++ pi_montecarlo.cpp
@@ -9,12 +9,13 @@
  *
  * total_samples debe ser un entero largo (p.ej. 100000000)
  */
-
+ #include <iostream>
  #include <mpi.h>
  #include <stdio.h>
  #include <stdlib.h>
  #include <time.h>
  #include <omp.h>
+ #include <random>
  
  int main(int argc, char **argv) {//argc may be the total samples
      int rank, size;
@@ -30,7 +31,6 @@
      /* 2: Validar el argumento de entrada en el proceso 0. Enviar
                 total_samples a todos los procesos (MPI_Bcast)           */  
      if (rank == 0) {
-         total_samples = argc;
          for (int c = 1; c < size; c++) {
              MPI_Send(&total_samples, 1, MPI_INT, c, 0, MPI_COMM_WORLD);
          }
@@ -44,16 +44,17 @@
      std::uniform_real_distribution<> rng(0.0, 1.0); // Define the rng range
      /* 5: Generar points (x,y) aleatorios y contar los que caen dentro del círculo de radio 1.    */
      for(int i = 0; i < samples_per_proc; i++) {
-         //local_count += start + (rng(seed) * end);
+         //Obtain the (x,y) point
          start = rng(seed);
          end = rng(seed);
-         double div = start / end;
-         if (div < 1 && div > 0) {
-             local_count += div;
+         double dist = start * start + end * end;
+         //Check if it's in the circle'
+         if (dist <= 1) {
+             local_count++;
          }
      }
      /* 6: Reducir los conteos locales en total_count usando MPI_Reduce                                 */
-     MPI_Reduce(&local_count, &total_count, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD)
+     MPI_Reduce(&local_count, &total_count, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
      /* 7: En el proceso 0, calcular pi_estimate = 4 * total_count / total_samples y mostrar resultados */
      if (rank == 0) {
          double pi_estimate = 4 * (total_count / total_samples);
